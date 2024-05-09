8bfa208e689c1bf4d2c289328af39b0c0d949743
DipCoy <dipolarcoyote@gmail.com>
2024-05-09 17:12:40 +0500

    fix installation script

--- scripts/install-apt.sh
+++ scripts/install-apt.sh
@@ -1,5 +1,7 @@
 sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com D27D666CD88E42B4
-sudo apt-get update
-
 echo "deb [trusted=yes] https://mirror.yandex.ru/mirrors/elastic/8/ stable main" | sudo tee /etc/apt/sources.list.d/elastic-8.x.list
+
+sudo apt-get update
 sudo apt-get upgrade
+
+sudo apt-get install elasticsearch
